e665539ec64cae27332588af2bed11353eb948ae
David Taylor <david.a.taylor253@gmail.com>
2022-01-12 21:05:47 -0800

    added audio to asset manager, added key listeners to game engine, worked on dynamic movement/animation, added tashigi animation 2.

--- docs/assetmanager.js
+++ docs/assetmanager.js
@@ -21,27 +21,72 @@ class AssetManager {
             const img = new Image();
 
             const path = this.downloadQueue[i];
-            console.log(path);
+            var ext = path.substring(path.length - 3);
+            
+            if(ext === 'png'){
+                console.log(path);
 
-            img.addEventListener("load", () => {
-                console.log("Loaded " + img.src);
-                this.successCount++;
+                img.addEventListener("load", () => {
+                    console.log("Loaded " + img.src);
+                    this.successCount++;
                 if (this.isDone()) callback();
-            });
+                });
 
-            img.addEventListener("error", () => {
-                console.log("Error loading " + img.src);
-                this.errorCount++;
-                if (this.isDone()) callback();
-            });
+                img.addEventListener("error", () => {
+                    console.log("Error loading " + img.src);
+                    this.errorCount++;
+                    if (this.isDone()) callback();
+                });
 
-            img.src = path;
-            this.cache[path] = img;
+                img.src = path;
+                this.cache[path] = img;
+            } else if(ext === 'mp3'){
+                var aud = new Audio();
+                aud.addEventListener("loadeddata", function() {
+                    console.log("Loaded " + this.src);
+                    that.successCount++;
+                    if(that.isDone()) callback();
+                });
+
+                aud.addEventListener("error", function () {
+                    console.log("Error loading " + this.src);
+                    that.errorCount++;
+                    if(that.isDone()) callback();
+                });
+
+                aud.addEventListener("ended", function() {
+                    aud.pause();
+                    aud.currentTime = 0; //ready to run audio again.
+                });
+
+                aud.src = path;
+                aud.load();
+
+                this.cache[path] = aud;
+
+            } else {
+                console.log("Error loading, extention was neither jpg or png!");
+            }
+
+            
         }
     };
 
     getAsset(path) {
         return this.cache[path];
+    };
+
+    playAsset(path) {
+        let audio = this.cache[path];
+        audio.currentTime = 0;
+        audio.play();
+    };
+
+    autoRepeat(path) {
+        var aud = this.cache[path];
+        aud.addEventListener("ended", function(){ //listeners for an event will fire off in added order.
+            aud.play();
+        });
     };
 };
 
--- docs/main.js
+++ docs/main.js
@@ -2,29 +2,34 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
-ASSET_MANAGER.queueDownload("./Tashigi-walking.png");
-ASSET_MANAGER.queueDownload("./Tashigi-walking-right.png");
-ASSET_MANAGER.queueDownload("./Tashigi-hori-slash.png");
-ASSET_MANAGER.queueDownload("./Tashigi-hori-slash-right.png");
-ASSET_MANAGER.queueDownload("./Tashigi-vertical.png");
-ASSET_MANAGER.queueDownload("./Tashigi-vertical-right.png");
-ASSET_MANAGER.queueDownload("./Tashigi-upslash.png");
-ASSET_MANAGER.queueDownload("./Tashigi-upslash-right.png");
-ASSET_MANAGER.queueDownload("./Tashigi-speedslash.png");
-ASSET_MANAGER.queueDownload("./Tashigi-sheath-strike.png");
-ASSET_MANAGER.queueDownload("./Tashigi-idle.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-walking.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-walking-right.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-hori-slash.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-hori-slash-right.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-vertical.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-vertical-right.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-upslash.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-upslash-right.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-speedslash.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-sheath-strike.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi-idle.png");
 
 
 
-ASSET_MANAGER.queueDownload("./Tashigi.png");
+ASSET_MANAGER.queueDownload("./docs/Tashigi.png");
+//I had to adjust all of my links so that it would work in github pages.
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
 
+	//ASSET_MANAGER.autoRepeat("theme music");
+
 	gameEngine.addEntity(new Tashigi(gameEngine, 1, 0, 0, 0));
 	gameEngine.addEntity(new Tashigi(gameEngine, 1, 1, 0, 100)); //okay I can now create multiple tashigis!
+	gameEngine.addEntity(new Tashigi(gameEngine, 2, 0, 350, 600)); //okay I can now create multiple tashigis!
+	gameEngine.addEntity(new Tashigi(gameEngine, 2, 1, 650, 650)); //okay I can now create multiple tashigis!
 	gameEngine.addEntity(new Tashigi(gameEngine, 5, 1, 0, 550));  //fixed flicker by restoring canvas after scale call.
 	gameEngine.addEntity(new Tashigi(gameEngine, 5, 0, 0, 400)); 
 	gameEngine.addEntity(new Tashigi(gameEngine, 3, 0, 0, 250));
